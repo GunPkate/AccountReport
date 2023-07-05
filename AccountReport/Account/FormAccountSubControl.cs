d300c1c66b586418d65ff1128faca896f456da02
GunPKate <gunPK>
2023-07-05 20:47:22 +0700

    Add project files.

---
+++ AccountReport/Account/FormAccountSubControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountReport.Account
+{
+    public partial class FormAccountSubControl : Form
+    {
+        public FormAccountSubControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AccountReport/Main/MainForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AccountReport.Account;
+using AccountReport.Purchase;
+using AccountReport.Stock;
+using AccountReport.User;
+
+namespace AccountReport
+{
+    public partial class mainForm : Form
+    {
+        formUserTypes UserTypesForm;
+        formAddUser AddUserForm;
+        formProductDetails Add_ProductForm;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            btnLogin.Visible = true;
+            btnLogout.Visible = false;
+            menuStripAll.Enabled = false;
+        }
+
+        private void userTypesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (UserTypesForm == null)
+            {
+                UserTypesForm = new formUserTypes();
+            }
+            UserTypesForm.ShowDialog();
+        }
+
+        private void addUsersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (AddUserForm == null)
+            {
+                AddUserForm = new formAddUser();
+            }
+            AddUserForm.ShowDialog();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            formLogin formLogin = new formLogin(this);
+            formLogin.ShowDialog();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            btnLogin.Visible = true;
+            btnLogout.Visible = false;
+            menuStripAll.Enabled = false;
+        }
+
+        private void updateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formUpdateUser updateUser = new formUpdateUser();
+            updateUser.ShowDialog();
+        }
+
+        private void categoriesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formCategories formCategories = new formCategories();
+            formCategories.ShowDialog(); 
+        }
+
+        private void productToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Add_ProductForm == null) {
+                Add_ProductForm = new formProductDetails();
+            }
+            Add_ProductForm.TopLevel = false;
+            panelParent.Controls.Add(Add_ProductForm);
+            Add_ProductForm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            Add_ProductForm.Dock = DockStyle.Fill;
+            Add_ProductForm.Show();
+        }
+
+        private void accountHeadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormAccountHead formAccountHead = new FormAccountHead();
+            formAccountHead.Show();
+        }
+
+        private void accountControlToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormAccountControl formAccountControl = new FormAccountControl();
+            formAccountControl.ShowDialog();
+        }
+
+        private void accountSubControlToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormAccountSubControl formAccountSubControl = new FormAccountSubControl();
+            formAccountSubControl.ShowDialog();
+        }
+
+        private void financialYearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormFinancialYear formFinancialYear = new FormFinancialYear();
+            formFinancialYear.ShowDialog();
+        }
+
+        private void supplierToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormSupplier formSupplier = new FormSupplier();
+            formSupplier.ShowDialog();
+        }
+
+        private void customerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormCustomer formCustomer = new FormCustomer();
+            formCustomer.ShowDialog();
+        }
+    }
+}
